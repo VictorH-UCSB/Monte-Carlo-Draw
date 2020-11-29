db223265d5eaf136dacb2ca2b6fc755aebcf1459
VictorH-UCSB <chuanhanhuang@ucsb.edu>
2020-11-29 18:10:25 +0800

    Research update

--- functions/simulation2.R
+++ functions/simulation2.R
@@ -12,7 +12,8 @@ simulation2<- function(cap_y,x,p,nSims){
     capy_select<-cap_y[sample]
     
 
-    beta_hat<-lm(formula=capy_select~x_select+0)[[1]] %>% as.numeric()
+    #beta_hat<-lm(formula=capy_select~x_select+0)[[1]] %>% as.numeric()
+    beta_hat<-sum(capy_select*x_select)/sum(x_select**2)
     out[i]<-beta_hat
   }
   return(out)
